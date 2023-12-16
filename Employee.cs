2cff77b771861f2078a5d852fdd66569aff8b5fa
NaderMamdouh99 <Nader.mamdouh9898@gmail.com>
2023-12-16 17:09:59 +0200

    Add project files.

---
+++ Employee.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3
+{
+    public class Employee
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+        public int Salary { get; set; }
+        public string Gender { get; set; }
+        public int Experience { get; set; }
+
+
+        public Employee(int _id, string _Name, int _Salary,string _Gender,int _Experience)
+        {
+            this.id = _id;
+            this.Name = _Name;
+            this.Salary = _Salary;
+            this.Gender = _Gender;
+            this.Experience = _Experience;
+
+            
+        }
+        public override string ToString()
+        {
+            return $"Id = {id} : Name = {Name} : Salary = {Salary} : Gender = {Gender} : Experience = {Experience} :";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Linq.Expressions;
+using System.Runtime.Intrinsics.X86;
+
+namespace Day3
+{
+    internal class Program
+    {
+            #region Delegate
+            public delegate bool DelagateEmp(Employee empID);
+
+            public static bool PromotionSalary(Employee Emp)
+            {
+                return Emp.Salary < 10000;
+            }
+            public static bool PromotionExperience(Employee Emp)
+            {
+                return Emp.Experience > 5;
+            }
+            public static List<Employee> PromotionEmployees(List<Employee> employees, DelagateEmp delagateEmp)
+            {
+                List<Employee> Result = new List<Employee>();
+                foreach (var item in employees)
+                {
+                    if (delagateEmp(item))
+                        Result.Add(item);
+                }
+                return Result;
+            } 
+        #endregion
+
+        static void Main(string[] args)
+        {
+			#region Assignment
+                //1.Create a class Employee that has the following properties:
+                    //a.Id.
+                    //		b.Name.
+                    //		c.Salary.
+                    //d.Gender.
+                    //e.Experience.
+                //2.Make a function Promote that return a list of promoted Employees according to PromotionCriteria Function.
+                //3.PromotionCriteria is a function with the following signature
+                    //Public Static bool PromotionCriteria(Employee emp)
+                    //That returns whether the emp should be promoted or not.
+                //4.	Create 2 Promotion Criteria:
+                    //a.One if the salary is under 10000.
+                    //b.One if the experience years are above 5.
+                //5.	Try to use Lambda expression.
+
+		#endregion
+
+            #region Lab
+
+			List<Employee> list = new List<Employee>();
+            list.Add(new Employee(10, "Nader", 15000, "M", 6));
+            list.Add(new Employee(11, "Nada", 5000, "F", 4));
+            list.Add(new Employee(12, "Aya", 8000, "F", 3));
+            list.Add(new Employee(14, "Ali", 18000, "M", 7));
+            DelagateEmp Del = PromotionSalary;
+            DelagateEmp Del2 = PromotionExperience;
+
+            Console.WriteLine("a.\tOne if the salary is under 10000");
+            foreach (var item in PromotionEmployees(list, Del))
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("b.\tOne if the experience years are above 5.");
+            foreach (var item in PromotionEmployees(list, Del2))
+            {
+                Console.WriteLine(item);
+            }
+            PromotionEmployees(list, E => E.Experience > 3); 
+            #endregion
+        }
+    }
+}
